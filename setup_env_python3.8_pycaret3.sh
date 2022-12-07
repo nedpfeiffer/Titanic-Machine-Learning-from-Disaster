9b60beb7f2515b5d804db193ecbb1660e5ca5d4c
nedpfeiffer <116780681+nedpfeiffer@users.noreply.github.com>
2022-12-07 10:25:53 -0700

    Add files via upload

---
+++ setup_env_python3.8_pycaret3.sh
@@ -0,0 +1,12 @@
+sudo apt-get update;
+sudo apt-get install -y python3.8;
+sudo apt-get install -y python3.8-distutils;
+sudo add-apt-repository ppa:deadsnakes/ppa;
+sudo unlink /usr/bin/python3 && sudo ln -s /usr/bin/python3.8 /usr/bin/python3;
+sudo unlink /usr/bin/python && sudo ln -s /usr/bin/python3 /usr/bin/python;
+sudo pip3 install --upgrade setuptools;
+sudo pip3 install --upgrade pip;
+sudo pip3 install --upgrade distlib;
+# --pre and [full] were used when pycaret3 wasnt the latest.
+# ignore pexpect was added due to an error when pip couldnt uninstall pexpect
+sudo pip3 install --pre pycaret --ignore-installed pexpect;
